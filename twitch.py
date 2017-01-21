dbf3611d4c41e25b88f03aba56f771abf5eb7c63
Maddosaurus <mail@maddosaurus.de>
2017-01-22 00:08:41 +0100

    Introduce detailed stats for individual subs
    danger! Currently not checking/removing oboslete subs!

--- twitch.py
+++ twitch.py
@@ -6,12 +6,12 @@ def prime(bot, trigger):
     if(not bot.memory.contains(trigger.sender)):
         bot.memory[trigger.sender] = {}
     bot.say('Found a Prime Subscriber! -> ' + trigger.split(" ")[0], 'maddosaurus')
-    bot.memory[trigger.sender].update(create_info(trigger, 'prime'))
+    bot.memory[trigger.sender].update(create_info(trigger, 'P'))
 
 @module.rule('(\w*) just subscribed!')
 def paid(bot, trigger):
     bot.say('Got a Paid Subscriber! -> ' + trigger.split(" ")[0], 'maddosaurus')
-    bot.memory[trigger.sender].update(create_info(trigger, 'paid'))
+    bot.memory[trigger.sender].update(create_info(trigger, '$'))
 
 @module.commands('twstats')
 def twstats(bot, trigger):
@@ -23,6 +23,16 @@ def twstats(bot, trigger):
     bot.say('Subcounter for ' + channel + ": " + str(len(bot.memory[channel].keys())))
     bot.say('Subs: ' + ", ".join(bot.memory[channel].keys()))
 
+@module.commands('twdetail')
+def twdetail(bot, trigger):
+    if(not trigger.admin):
+        bot.say('Sorry. I am only answering my admins this question.')
+        return
+    channel = trigger.group(2)
+    bot.say('Preparing detailed stats for the channel: ' + channel)
+    bot.say('Legend: [P] is an Amazon Prime Sub, [$] a paid sub')
+    for key, values in bot.memory[channel].items():
+        bot.say('[' + values['type'] + ']' + key +', Subbed: ' + values['date'].strftime("%Y-%m-%d"))
 
 # Helpers
 
