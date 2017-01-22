f24be37d84d8eece94bb0a28b64714360247bc03
Maddosaurus <mail@maddosaurus.de>
2017-01-22 19:00:43 +0100

    Extend the bot and change settings
    The module will now only interact with privmessages by its admins.
    Also reworked the commands again. But they need more love

--- twitch.py
+++ twitch.py
@@ -5,34 +5,50 @@ from datetime import datetime, timedelta
 def prime(bot, trigger):
     if(not bot.memory.contains(trigger.sender)):
         bot.memory[trigger.sender] = {}
-    bot.say('Found a Prime Subscriber! -> ' + trigger.split(" ")[0], 'maddosaurus')
+    bot.say('['+ trigger.sender +']: Found a Prime Subscriber! -> ' + trigger.split(" ")[0], 'maddosaurus')
     bot.memory[trigger.sender].update(create_info(trigger, 'P'))
 
 @module.rule('(\w*) just subscribed!')
 def paid(bot, trigger):
-    bot.say('Got a Paid Subscriber! -> ' + trigger.split(" ")[0], 'maddosaurus')
+    if(not bot.memory.contains(trigger.sender)):
+        bot.memory[trigger.sender] = {}
+    bot.say('['+ trigger.sender +']: Got a Paid Subscriber! -> ' + trigger.split(" ")[0], 'maddosaurus')
     bot.memory[trigger.sender].update(create_info(trigger, '$'))
 
+@module.require_privmsg
+@module.require_admin
 @module.commands('twstats')
 def twstats(bot, trigger):
-    if(not trigger.admin):
-        bot.say('Sorry. I am only answering my admins this question.')
+    if(trigger.group(2) is None):
+        bot.say('Usage: .twdetail #channelname')
         return
     channel = trigger.group(2)
     bot.say('Subcounter for ' + channel + ": " + str(len(bot.memory[channel].keys())))
-    bot.say('Subs: ' + ", ".join(bot.memory[channel].keys()))
+    bot.say('Subs: ' + ", ".join(bot.memory[channel].keys())) # Debug output! This will be too much on regular basis
 
+@module.require_privmsg
+@module.require_admin
 @module.commands('twdetail')
 def twdetail(bot, trigger):
-    if(not trigger.admin):
-        bot.say('Sorry. I am only answering my admins this question.')
+    if(trigger.group(2) is None):
+        bot.say('Usage: .twdetail #channelname')
         return
     channel = trigger.group(2)
+    if(not bot.memory.contains(channel)):
+        bot.say('No stats found for this channel. Please wait for the first sub')
+        return
+    
+    # FIXME: Freakin' UTC time stuff driving me crazy -_-
+    dtnow = datetime.now()
+    dtutc = datetime.utcnow()
+    delta = dtnow -dtutc
+    # done with the time blackmagic - just addin' this on the subdate. Dammit!
+
     bot.say('Preparing detailed stats for the channel: ' + channel)
-    bot.say('Legend: [P] is an Amazon Prime Sub, [$] a paid sub - WARN: Sub Hours are note calculated correctly ATM!')
+    bot.say('Legend: [P] is an Amazon Prime Sub, [$] a paid sub')
     bot.say('______________________________________________')
     for key, values in bot.memory[channel].items():
-        bot.say('[' + values['type'] + ']' + key +', Subbed: ' + values['date'].strftime("%Y-%m-%d : %H:%M:%S") + 'Has been subbed for '+ str((datetime.now() - values['date']).days) + ' days and ' + str((datetime.now() - values['date']).seconds/3600) + ' hours')
+        bot.say('[' + values['type'] + ']' + key +', Subbed: ' + (values['date'] + delta).strftime("%Y-%m-%d : %H:%M:%S")  + ' || Has been subbed for '+ str((datetime.now() - values['date']).days) + ' days, or roughly ' + str(int((datetime.utcnow() - values['date']).seconds/60)) + ' minutes')
     bot.say('______________________________________________')
     bot.say('For the completionzz: Output of twstats')
     twstats(bot, trigger)
