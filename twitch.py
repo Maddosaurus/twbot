7519024b0f7f402ab751d5ea6cea56964cae42f7
Maddosaurus <mail@maddosaurus.de>
2017-01-22 20:25:37 +0100

    Refined the bot some more and added twsubs command

--- twitch.py
+++ twitch.py
@@ -1,5 +1,5 @@
 from sopel import module
-from datetime import datetime, timedelta
+from datetime import datetime
 
 @module.rule('(\w*) just subscribed with Twitch Prime!')
 def prime(bot, trigger):
@@ -15,43 +15,37 @@ def paid(bot, trigger):
     bot.say('['+ trigger.sender +']: Got a Paid Subscriber! -> ' + trigger.split(" ")[0], 'maddosaurus')
     bot.memory[trigger.sender].update(create_info(trigger, '$'))
 
+
 @module.require_privmsg
 @module.require_admin
 @module.commands('twstats')
 def twstats(bot, trigger):
-    if(trigger.group(2) is None):
-        bot.say('Usage: .twdetail #channelname')
-        return
     channel = trigger.group(2)
-    bot.say('Subcounter for ' + channel + ": " + str(len(bot.memory[channel].keys())))
-    bot.say('Subs: ' + ", ".join(bot.memory[channel].keys())) # Debug output! This will be too much on regular basis
-
-@module.require_privmsg
-@module.require_admin
-@module.commands('twdetail')
-def twdetail(bot, trigger):
     if(trigger.group(2) is None):
-        bot.say('Usage: .twdetail #channelname')
+        bot.say('Usage: .twstats #channelname - Print stat overview for given channel')
         return
-    channel = trigger.group(2)
     if(not bot.memory.contains(channel)):
         bot.say('No stats found for this channel. Please wait for the first sub')
         return
     
-    # FIXME: Freakin' UTC time stuff driving me crazy -_-
-    dtnow = datetime.now()
-    dtutc = datetime.utcnow()
-    delta = dtnow -dtutc
-    # done with the time blackmagic - just addin' this on the subdate. Dammit!
+    bot.say('Eyeballing subs for ' + channel + ": ")
+    bot.say(buildStats(bot, channel)) 
 
-    bot.say('Preparing detailed stats for the channel: ' + channel)
-    bot.say('Legend: [P] is an Amazon Prime Sub, [$] a paid sub')
-    bot.say('______________________________________________')
-    for key, values in bot.memory[channel].items():
-        bot.say('[' + values['type'] + ']' + key +', Subbed: ' + (values['date'] + delta).strftime("%Y-%m-%d : %H:%M:%S")  + ' || Has been subbed for '+ str((datetime.now() - values['date']).days) + ' days, or roughly ' + str(int((datetime.utcnow() - values['date']).seconds/60)) + ' minutes')
-    bot.say('______________________________________________')
-    bot.say('For the completionzz: Output of twstats')
-    twstats(bot, trigger)
+@module.require_privmsg
+@module.require_admin
+@module.commands('twsubs')
+def twsubs(bot, trigger):
+    channel = trigger.group(2)
+    if(trigger.group(2) is None):
+        bot.say('Usage: .twsubs #channelname - list *all* subscribers of the given channel. Be warned! This may be big output!')
+        return
+    if(not bot.memory.contains(channel)):
+        bot.say('No stats found for this channel. Please wait for the first sub')
+        return
+    
+    bot.say('Listing all subs for channel ' + channel)
+    bot.say(buildPrint(bot, channel)) 
+
 
 # Helpers
 
@@ -64,3 +58,29 @@ def create_info(trigger, subtype):
     ret[subber]['date'] = trigger.time
 
     return ret
+
+def buildPrint(bot, channel):
+    subs = ""
+    for key, values in bot.memory[channel].items():
+        subs += '[' + values['type'] + ']' + key +'('+  values['date'].strftime("%Y-%m-%d") +'), '
+    return subs
+
+# Bear in mind! I'm eyeballing this right here.
+# You can sub in 3 oder 6 Month periods. I don't know how common this is.
+# Also, the resub grace time is 30 days for your streak. 
+# So it would be 60 days for a monthly sub!
+# This stats will never be accurate, sorry :/
+def buildStats(bot, channel):
+    paid = 0
+    prime = 0
+    subs = 0
+    for key, values in bot.memory[channel].items():
+        if((datetime.now() - values['date']).days < 37):
+           subs+=1
+        else:
+            break
+        if (values['type'] == '$'):
+            paid+=1
+        elif (values['type'] == 'P'):
+            prime+=1
+    return "All in all: " + str(subs) + " -- Paid: "+ str(paid/subs*100) + "% (" + str(paid) + ") -- Twitch Prime: " + str(prime/subs*100) +"% ("+ str(prime) + ")"
